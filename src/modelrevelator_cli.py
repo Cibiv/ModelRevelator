2a7b684b0f109e6496b12178a30cfaab966fbbc2
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2023-03-26 14:27:40 +0200

    added fasta file support

--- src/modelrevelator_cli.py
+++ src/modelrevelator_cli.py
@@ -80,104 +80,118 @@ def process_msa_pairwise(lines):
             print('Duration MSA remapping', time.time() - start)
 
         for c in range(0, num_sumstats):
-            # make sure that two different strands are being selected
-            while True:
-                first_strand = np.random.randint(0, m.shape[1], size=1, dtype=int)[0]
-                second_strand = np.random.randint(0, m.shape[1], size=1, dtype=int)[0]
 
-                if first_strand != second_strand:
+            # With gapped MSAs, we cannot be sure to have sequence pairs of length > 0 after gap removal.
+            # So try 1000 times to get such a pair. Else, return zeros.
+            try_counter = 0
+            while True:
+                if try_counter >= 1000:
                     break
 
-            fs = m[:, first_strand]
-            ss = m[:, second_strand]
+                try_counter += 1
 
-            # print(ss.shape)
+                # make sure that two different strands are being selected
+                while True:
+                    first_strand = np.random.randint(0, m.shape[1], size=1, dtype=int)[0]
+                    second_strand = np.random.randint(0, m.shape[1], size=1, dtype=int)[0]
 
-            # For now, remove gaps and degenerate characters
-            mask = (fs != 0) & (ss != 0)
-            fs = fs[mask]
-            ss = ss[mask]
+                    if first_strand != second_strand:
+                        break
 
-            fs = fs.reshape((fs.shape[0], 1))
-            ss = ss.reshape((ss.shape[0], 1))
+                fs = m[:, first_strand]
+                ss = m[:, second_strand]
 
-            # make sure to normalize to new lenght after removal
-            msa_length = len(fs)
+                # print(ss.shape)
 
-            # Fist strand base counts calculation
-            start = time.time()
+                # For now, remove gaps and degenerate characters
+                mask = (fs != 0) & (ss != 0)
+                fs = fs[mask]
+                ss = ss[mask]
 
-            for ccc, x in enumerate(msa_int_map.values()):
-                stat = np.sum(fs == x)
-                mm[c, ccc + 4] = stat / msa_length
+                fs = fs.reshape((fs.shape[0], 1))
+                ss = ss.reshape((ss.shape[0], 1))
 
-            if benchmark:
-                print('Duration FSBC opt:', time.time() - start)
+                # make sure to normalize to new length after removal
+                msa_length = len(fs)
+                if msa_length <= 0:
+                    continue
 
-            # Second strand base counts calculation
-            start = time.time()
- 
-            for ccc, x in enumerate(msa_int_map.values()):
-                stat = np.sum(ss == x)
-                mm[c, ccc + 8] = stat / msa_length
+                # Fist strand base counts calculation
+                start = time.time()
 
-            if benchmark:
-                print('Duration SSBC opt:', time.time() - start)
+                for ccc, x in enumerate(msa_int_map.values()):
+                    stat = np.sum(fs == x)
+                    mm[c, ccc + 4] = stat / msa_length
 
-            # Transition and Transversion counts
-            start = time.time()
-            t = fs << ss
-            tts = [
-                1 << 1,  # 2 AA,
-                1 << 3,  # 8 AC
-                1 << 5,  # 32 AG
-                1 << 7,  # 128 AT
-                3 << 1,  # 6 CA
-                3 << 3,  # 24 CC
-                3 << 5,  # 96 CG
-                3 << 7,  # 384 CT
-                5 << 1,  # 10 GA
-                5 << 3,  # 40 GC
-                5 << 5,  # 160 GG
-                5 << 7,  # 640 GT
-                7 << 1,  # 14 TA
-                7 << 3,  # 56 TC
-                7 << 5,  # 224 TG
-                7 << 7,  # 896 TT
-            ]
-            for x in [np.uint16(y) for y in tts]:
-                stat = np.sum(t == x)
-                ind = tstv_map[x]
-                mm[c, ind] = stat / msa_length
+                if benchmark:
+                    print('Duration FSBC opt:', time.time() - start)
 
-            if benchmark:
-                print('Duration tstv counts:', time.time() - start)
+                # Second strand base counts calculation
+                start = time.time()
 
-            # Count transversions only
-            start = time.time()
-            transversion_counts = 0
-            for t in [1 << 3, 3 << 1, 1 << 7, 7 << 1, 3 << 5, 5 << 3, 5 << 7, 7 << 5]:
-                # ['AC', 'CA', 'AT', 'TA', 'CG', 'GC', 'GT', 'TG']:
-                ind = tstv_map[t]
-                transversion_counts += mm[c, ind]
+                for ccc, x in enumerate(msa_int_map.values()):
+                    stat = np.sum(ss == x)
+                    mm[c, ccc + 8] = stat / msa_length
 
-            mm[c, 12] = transversion_counts  # already normalized counts
-            
-            if benchmark:
-                print("Duration transversion counts", time.time() - start)
+                if benchmark:
+                    print('Duration SSBC opt:', time.time() - start)
 
-            # Count transitions only
-            start = time.time()
-            transition_counts = 0
-            for t in [1 << 5, 5 << 1, 3 << 7, 7 << 3]:
-                # ['AG', 'GA', 'CT', 'TC']:
-                ind = tstv_map[t]
-                transition_counts += mm[c, ind]
+                # Transition and Transversion counts
+                start = time.time()
+                t = fs << ss
+                tts = [
+                    1 << 1,  # 2 AA,
+                    1 << 3,  # 8 AC
+                    1 << 5,  # 32 AG
+                    1 << 7,  # 128 AT
+                    3 << 1,  # 6 CA
+                    3 << 3,  # 24 CC
+                    3 << 5,  # 96 CG
+                    3 << 7,  # 384 CT
+                    5 << 1,  # 10 GA
+                    5 << 3,  # 40 GC
+                    5 << 5,  # 160 GG
+                    5 << 7,  # 640 GT
+                    7 << 1,  # 14 TA
+                    7 << 3,  # 56 TC
+                    7 << 5,  # 224 TG
+                    7 << 7,  # 896 TT
+                ]
+                for x in [np.uint16(y) for y in tts]:
+                    stat = np.sum(t == x)
+                    ind = tstv_map[x]
+                    mm[c, ind] = stat / msa_length
 
-            mm[c, 13] = transition_counts  # already normalized
+                if benchmark:
+                    print('Duration tstv counts:', time.time() - start)
 
-            if benchmark:
-                print('Duration transition counts', time.time() - start)
+                # Count transversions only
+                start = time.time()
+                transversion_counts = 0
+                for t in [1 << 3, 3 << 1, 1 << 7, 7 << 1, 3 << 5, 5 << 3, 5 << 7, 7 << 5]:
+                    # ['AC', 'CA', 'AT', 'TA', 'CG', 'GC', 'GT', 'TG']:
+                    ind = tstv_map[t]
+                    transversion_counts += mm[c, ind]
+
+                mm[c, 12] = transversion_counts  # already normalized counts
+
+                if benchmark:
+                    print("Duration transversion counts", time.time() - start)
+
+                # Count transitions only
+                start = time.time()
+                transition_counts = 0
+                for t in [1 << 5, 5 << 1, 3 << 7, 7 << 3]:
+                    # ['AG', 'GA', 'CT', 'TC']:
+                    ind = tstv_map[t]
+                    transition_counts += mm[c, ind]
+
+                mm[c, 13] = transition_counts  # already normalized
+
+                if benchmark:
+                    print('Duration transition counts', time.time() - start)
+
+                break
 
         return mm
 
@@ -250,16 +264,33 @@ for count, msa_file in enumerate(msa_files):
 
     with open(msa_file, 'rb') as f:
         lines = f.readlines()
-        
         proc_start_time = time.time()
-        raw_msa = np.array([x.decode().strip().split(' ')[-1] for x in lines[1:]])
-        # start = time.time()
+
+        if msa_file.endswith('.phy'):
+            raw_msa = np.array([x.decode().strip().split(' ')[-1] for x in lines[1:]])
+            # start = time.time()
+
+        elif msa_file.endswith('.fasta'):
+            sub_seq = []
+            all_lines = []
+            for l in lines:
+                if l.startswith(b'>'):
+                    if len(sub_seq) > 0:
+                        all_lines.append(''.join(sub_seq))
+                    # print(''.join(sub_seq))
+                    sub_seq = []
+                else:
+                    sub_seq.append(l.decode().strip().upper())
+
+            raw_msa = np.array(all_lines)
+            print(raw_msa)
         proc_model_msa = process_msa_pairwise(raw_msa)
         # print('pairwise prepro duration', time.time() - start)
 
         # start = time.time()
         proc_alpha_msa = process_alpha_msa(raw_msa)
         # print('columwise preproc duration:', time.time() - start)
+
     
     proc_end_time = time.time()
     all_results['duration_preprocessing'].append(proc_end_time - proc_start_time) 
